55019766f70b8b433f4df29c548b480c14bbf6cf
r0b1337 <master@r0b.me>
2021-07-03 23:45:53 +0200

    feat(Mower): :building_construction: constructor + position/direction properties

--- Mower.ts
+++ Mower.ts
@@ -1,2 +1,15 @@
+import IPosition from "./IPosition";
+import { Direction } from "./types";
+
 export default class Mower {
+    public position: IPosition;
+    public direction: Direction;
+
+    constructor(
+        position: IPosition,
+        direction: Direction,
+    ) {
+        this.position = position;
+        this.direction = direction;
+    }
 }
