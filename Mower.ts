499c52fcaa9053f29424db4af7bd5ce9cc00f99b
r0b1337 <master@r0b.me>
2021-07-03 23:46:32 +0200

    feat(Mower): :sparkles: process instructions
    ]

--- Mower.ts
+++ Mower.ts
@@ -8,8 +8,26 @@ export default class Mower {
     constructor(
         position: IPosition,
         direction: Direction,
+        instructions: string[] = [],
     ) {
         this.position = position;
         this.direction = direction;
+
+        instructions.forEach(instruction => this.processInstruction(instruction));
+    }
+
+    private processInstruction(instruction: string): void {
+        const to = {
+            L: { N: 'W', E: 'N', W: 'S', S: 'E' }, // directions rotated to the left
+            R: { N: 'E', E: 'S', W: 'N', S: 'W' }, // directions rotated to the right
+        } as any;
+
+        if (['L', 'R'].includes(instruction)) this.direction = to[instruction][this.direction];
+        else if (instruction === 'F') {
+            if (this.direction === 'N' && this.position.y < global.grid.x) this.position.y += 1;
+            else if (this.direction === 'E' && this.position.x < global.grid.y) this.position.x += 1;
+            else if (this.direction === 'S' && this.position.y > 0 ) this.position.y -= 1;
+            else if (this.direction === 'W' && this.position.x > 0) this.position.x -= 1;
+        }
     }
 }
