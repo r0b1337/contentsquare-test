5caa805fc2b15f94ae574bbd027d0cbba9ec83dc
r0b1337 <dev@r0b.me>
2021-07-03 18:27:33 +0200

    feat: :sparkles: input file parsing

--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
-console.log('process.argv :>> ', process.argv);
+const fs = require('fs');
+
+const parseInput = (lines: string[]): any => {
+    if (!/^[0-9]+ [0-9]+$/.test(lines[0])) throw 'Invalid grid coordinates';
+
+    const grid = lines.shift().split(' ').map((n: string) => parseInt(n));
+    const mowers: any[] = [];
+
+    lines.forEach((line: string, i: number) => {
+        if (i % 2) return;
+        if (!/^[0-9]+ [0-9]+ [NEWS]$/.test(line)) throw 'Invalid mower starting coords';
+
+        const state = line.split(' ');
+        const instructions = lines[i + 1];
+
+        mowers.push({state, instructions});
+    });
+
+    return { grid, mowers };
+}
+
+const main = (): void => {
+    try {
+        const inputFilename: string = process.argv[2];
+
+        if (!inputFilename) throw 'Missing input filename';
+
+        const input: string = fs.readFileSync(inputFilename, { encoding: 'utf8' });
+        const lines = input.split('\n');
+        const { grid, mowers } = parseInput(lines);
+
+        console.log('grid :>> ', grid);
+        console.log('mowers :>> ', mowers);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+main();
