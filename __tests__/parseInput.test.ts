70fd6987d7b49e358cb2367e9c5a0a1409936898
r0b1337 <master@r0b.me>
2021-07-04 17:07:50 +0200

    test(parseInput): :white_check_mark: should throw 'Invalid mower starting coords'

--- __tests__/parseInput.test.ts
+++ __tests__/parseInput.test.ts
@@ -9,4 +9,13 @@ describe('parseInput', () => {
     ])('should throw \'Invalid grid coordinates\' -- %j', (input: string[]) => {
         expect(() => parseInput(input)).toThrow('Invalid grid coordinates');
     });
+
+    it.each([
+        [['']],
+        [['1 2 Z']],
+        [['A 2 N']],
+        [[' 1 2 N   ']],
+    ])('should throw \'Invalid mower starting coords\' -- %j', (input: string[]) => {
+        expect(() => parseInput(['5 5', ...input])).toThrow('Invalid mower starting coords');
+    });
 });
