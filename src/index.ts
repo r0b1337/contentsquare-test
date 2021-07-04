c1188506fee5579b0ed352ac2a2ebc93b18ddc82
r0b1337 <master@r0b.me>
2021-07-04 19:18:02 +0200

    feat: :sparkles: windows support

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ const main = (): void => {
         if (!inputFilename) throw 'Missing input filename';
 
         const input: string = fs.readFileSync(inputFilename, { encoding: 'utf8' });
-        const lines = input.split('\n');
+        const separator = process.platform === 'win32' ? '\r\n' : '\n';
+        const lines = input.split(separator);
         const results = parseInput(lines);
 
         results.map((result: any) => new Mower(result.position, result.direction, result.instructions));
